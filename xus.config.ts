15a0076caa0f9882098dfbed4697dac7a54c53f8
guo.xu <xuguo@outlook.it>
2021-03-06 11:03:37 +0800

    chore: 项目init

---
+++ xus.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from '@xus/cli'
+
+export default defineConfig({
+  libBuild: {
+    targets: ['esm', 'cjs', 'browser'],
+    rollTypes: true
+  },
+  lint: {
+    eslint: {
+      include: './src/**/*',
+      ext: ['.ts']
+    },
+    stylelint: false
+  },
+  release: {
+    beforeRelease: [
+      {
+        bin: 'npm',
+        args: ['run', 'test'],
+        message: {
+          start: 'Test start',
+          succeed: 'Test succeed',
+          failed: 'Test failed'
+        }
+      },
+      {
+        bin: 'npm',
+        args: ['run', 'lint-fix'],
+        message: {
+          start: 'Lint start',
+          succeed: 'Lint succeed',
+          failed: 'Lint failed'
+        }
+      },
+      {
+        bin: 'npm',
+        args: ['run', 'build'],
+        message: {
+          start: 'Build start',
+          succeed: 'Build succeed',
+          failed: 'Build failed'
+        }
+      }
+    ],
+    branch: 'main'
+  }
+})
